41dd511e79eb3444ce60fe653777cfd64b4ca463
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-14 21:12:58 +0300

    Task07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -4,10 +4,10 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8
- Console.WriteLine("Введите трехзначное число: ");
+Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int result = 0;
-if (number > 0 && number < 1000)
+if (number > 99 && number < 1000)
 {
     result = number % 10;
     Console.WriteLine($"Последняя цифра числа: {result} ");
