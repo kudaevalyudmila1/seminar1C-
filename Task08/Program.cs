9659598a23b870a62ee48ed050ed09e59953f735
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-15 15:50:51 +0300

    Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -7,12 +7,11 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 int number = 1;
 while (number <= n)
-
 {
     if (number % 2 == 0)
     {
         Console.WriteLine(number);
-        number++;
+    
     }
-    else number++;
+    number++;
 }
