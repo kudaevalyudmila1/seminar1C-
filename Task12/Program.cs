16956f892da361da6dd953913c92173cf3e5c29b
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-18 20:41:54 +0300

    Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число:");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int second = Convert.ToInt32(Console.ReadLine());
+
+int Division(int num1, int num2)
+{
+    int result = num1 % num2;
+    return result;
+}
+int result = Division(first, second);
+if (result == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно, остаток {result}");
