dc24e65a2b4d3067867ade16d47564f01d250008
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-18 20:00:01 +0300

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число  {number}");
+
+int MinusMiddle(int num)
+{
+int first = num / 100;
+int second = num % 10;
+int result = first * 10 + second;
+return result;
+}
+int result = MinusMiddle(number);
+Console.WriteLine($"Число:  {result}");
+
