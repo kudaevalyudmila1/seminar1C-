2f887845528e10c3baa940037a885392d0e0d70d
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-18 23:12:40 +0300

    Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -10,13 +10,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int Dayweek(int num)
 {
-    if (num == 1) Console.WriteLine($"{num} - это понедельник");
-    else if (num == 2) Console.WriteLine($"{num} - это вторник");
-    else if (num == 3) Console.WriteLine($"{num} - это среда");
-    else if (num == 4) Console.WriteLine($"{num} - это четверг");
-    else if (num == 5) Console.WriteLine($"{num} - это пятница");
-    else if (num == 6) Console.WriteLine($"{num} - это суббота");
-    else if (num == 7) Console.WriteLine($"{num} - это воскресенье");
+    if (num == 6 || num == 7) Console.WriteLine($"{num} - это выходной день");
+    else Console.WriteLine($"{num} - этот день не является выходным");
+
     return num;
 }
 
