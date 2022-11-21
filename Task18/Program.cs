6b6b261a4dba2d9dd6bc38f8ad88b417a15643a2
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-21 22:11:33 +0300

    Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -7,7 +7,7 @@ string Range (int num)
     if (num == 2) return "x < 0, y > 0";
     if (num == 3) return "x > 0, y < 0";
     if (num == 4) return "x < 0, y < 0";
-    return "введен неверный номер четверти";
+    return "Введены некорректные данные";
 }
 string result = Range(number);
 Console.WriteLine($"Вероятный диапазон координат:{result}");
